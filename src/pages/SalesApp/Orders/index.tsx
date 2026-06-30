4ef424c081a5b7a63f28b070e2a214a3fef63e0c
Lucas <lucas@example.com>
2026-06-30 16:36:17 -0300

    feat: remove 'Importar Grupo' button from sales app orders list

--- src/pages/SalesApp/Orders/index.tsx
+++ src/pages/SalesApp/Orders/index.tsx
@@ -101,12 +101,6 @@ export default function SalesOrders() {
               <Plus className="h-4 w-4 mr-2" /> Criar pedido / orÃ§amento
             </Button>
           </Link>
-          
-          <Link to="/entregas/importar-grupo">
-            <Button variant="outline" className="text-emerald-600 border-emerald-500 hover:bg-emerald-50 font-bold px-4 h-10 shadow-sm rounded-md">
-              <Boxes className="h-4 w-4 mr-2" /> Importar Grupo
-            </Button>
-          </Link>
 
 
           
